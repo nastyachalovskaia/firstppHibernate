ee74e72c7ef47584881efebe618977d651dfa6f1
nastyachalovskaia <nastya.chalovskaia@gmail.com>
2022-08-17 08:49:09 +0300

    Удалила внутренний трай кэтч в методах, изменила нейминг

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -2,35 +2,34 @@ package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    UserDao ud = new UserDaoHibernateImpl();
+    UserDao userDao = new UserDaoHibernateImpl();
 
     public void createUsersTable() {
-        ud.createUsersTable();
+        userDao.createUsersTable();
     }
 
     public void dropUsersTable() {
-        ud.dropUsersTable();
+        userDao.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        ud.saveUser(name, lastName, age);
+        userDao.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        ud.removeUserById(id);
+        userDao.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return ud.getAllUsers();
+        return userDao.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        ud.cleanUsersTable();
+        userDao.cleanUsersTable();
     }
 }
